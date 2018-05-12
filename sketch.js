3cce27354f3f4b924e8ffc5dc46f3735dbf7bf50
shotyty <newhshoty@yahoo.com>
2018-05-12 19:39:27 -0400

    js test

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
-
-
-/*var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-fdeae6f2-aa9f-4ce4-b149-fef33c7b3428');
-ourRequest.onload = function() {
-	var ourData = JSON.parse(ourRequest.responseText);
-	console.log(ourData[0]);
+/*DON'T NEED THIS RIGHT NOW TO FILTER, MIGHT LATER
+var mageRole = document.getElementById('mage');
+var tankRole = document.getElementById('tank');*/
+var mageRole = document.getElementsByClassName('mage');
+tankRole = 'tank';
+var summonerName;
+var levelContainer = document.getElementById('level')
+var btn = document.getElementById('btn');
+var ourData;
+function grabInput() {
+	summonerName = document.getElementById('userInput').value;
+	document.getElementById('level').innerHTML = "";
 };
-ourRequest.send();*/
+function filterTank() {
+	for(var i = 0; i!= mageRole.length; ++i){
+		mageRole[i].style.display = "none";
+	};
 
-window.onload = function() {
-	var levelContainer = document.getElementById('level');
-	var btn = document.getElementById('btn');
-	var summonerName = 'shotyty';
-	var ourData;
-
-
+};
 
 	btn.addEventListener('click', function() {
+		document.getElementById('level').innerHTML = "";
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', "https://m99bp7llx2.execute-api.us-east-2.amazonaws.com/rgapi/summoner/na1/" + summonerName);
 		xhr.onload = function() {
@@ -27,7 +30,11 @@ window.onload = function() {
 	});
 
 	function renderHTML(data) {
-		levelContainer.insertAdjacentHTML('afterbegin', ourData.summonerLevel);
-	}
-
+		if(typeof ourData.summonerLevel == 'number') {
+	levelContainer.insertAdjacentHTML('beforeend', ourData.summonerLevel);
+} else {
+	levelContainer.insertAdjacentHTML('beforeend', 'NA');
 }
+};
+
+
